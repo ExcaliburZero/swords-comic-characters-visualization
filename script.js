2a291031051ab54cf8e0d393376d7fdc95e5d10a
Christopher Wells <cwellsny@nycap.rr.com>
2018-07-01 09:53:59 -0700

    Add character display window

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function main(config) {
-    $("#" + config.container).width(config.width);
+    $("#" + config.container).width(config.width - (config.displayWidth - 5));
     $("#" + config.container).height(config.height);
+
+    $(display).width(config.displayWidth);
+    $(display).height(config.height);
+    $(display).css({
+        position: "absolute",
+        top: "0px",
+        right: "0px",
+        background: "#cccccc",
+    });
 
     d3.csv(config.dataFile, data => {
         const edges = getCharacterEdges(data);
 
         let count = 0;
         const nodes = _(edges)
+            .map(row => row.characters)
             .flatten()
             .uniq()
             .map(name => {
@@ -15,19 +25,21 @@ function main(config) {
                     label: name,
                     image: "img/" + name + ".png",
                     shape: "image",
-                    value: _.filter(edges, r => r.includes(name)).length,
+                    //value: _.filter(edges, r => r.includes(name)).length,
                 };
             })
             .value();
 
         const getId = (name) => _.filter(nodes, n => n.label === name)[0].id;
         const edgeObjects = _(edges)
-            .uniqBy(row => "" + row)
+            .uniqBy(row => "" + row.characters)
             .map(row => {
                 return {
-                    from: getId(row[0]),
-                    to: getId(row[1]),
-                    value: _.filter(edges, r => _.isEqual(r, row)).length,
+                    from: getId(row.characters[0]),
+                    to: getId(row.characters[1]),
+                    value: _.filter(
+                        edges, r => _.isEqual(r.characters, row.characters)
+                    ).length,
                 };
             })
             .value();
@@ -39,6 +51,13 @@ function main(config) {
         };
         const options = {};
         const network = new vis.Network(container, nodesAndEdges, options);
+
+        network.on("selectNode", info => {
+            const nodeId = info.nodes[0];
+            const node = nodes[nodeId];
+
+            displayCharacter(node, config.display)
+        });
     });
 }
 
@@ -58,7 +77,10 @@ function getCharacterEdges(data) {
                 // Sort them so that direction doesn't matter
                 const sorted = [charA, charB].sort();
 
-                edges.push(sorted);
+                edges.push({
+                    comic: comic,
+                    characters: sorted,
+                });
             }
         }
     }
@@ -66,10 +88,23 @@ function getCharacterEdges(data) {
     return edges;
 }
 
+function displayCharacter(character, displayId) {
+    const display = $(displayId);
+
+    display.html(
+        "<h3 class=\"display-title\">" + character.label + "</h3>" +
+        "<img class=\"display-image\" src=\"" + character.image + "\">"
+    );
+
+    console.log(character);
+}
+
 const config = {
     dataFile: "Swords Comic - Appearances.csv",
     container: "characterGraph",
+    display: "#display",
     height: $(document).height() - 20,
     width: $(document).width() - 20,
+    displayWidth: 300,
 };
 main(config);
